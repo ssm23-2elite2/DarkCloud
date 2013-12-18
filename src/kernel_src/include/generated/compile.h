3a1dbd793b5cded0c8aa286e4ff5969c081217ee
Jake Yoon <yjaeseok@gmail.com>
2013-12-18 22:49:43 +0900

    Kernel Source Commit

---
+++ src/kernel_src/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Tue Dec 10 22:03:31 KST 2013"
+#define LINUX_COMPILE_BY "jake"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ src/kernel_src/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.31-g5894150"
